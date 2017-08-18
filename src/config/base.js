2d48e23b702bc5e524b0c474aa16638d976f1382
Damian Gattesco <gattex@DGATTESCO.local>
2017-08-18 10:16:35 -0300

    key de freetrial

--- src/config/base.js
+++ src/config/base.js
@@ -2,7 +2,7 @@
 
 // Settings configured here will be merged into the final config object.
 export default {
-	ACCOUNT_KEY: '',
+	ACCOUNT_KEY: '52ambWgGq4NLntiP8x8YUSPKj7MKEXGY',
 	// Set to 'docked' or 'normal' for docked or normal mode respectively
 	THEME: 'normal'
 }
